e015a846d978d24b9a4714e2a2e271eea1d03d81
Laura Ratti <lauralr759@gmail.com>
2023-09-18 23:35:22 +0200

    "BubbleSort"

--- src/main/java/com/laurartt02/exercises/arrays/BubbleSort.java
+++ src/main/java/com/laurartt02/exercises/arrays/BubbleSort.java
@@ -1,4 +1,26 @@
 package com.laurartt02.exercises.arrays;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void bubbleSort(int[] v){
+        boolean sorted = true;
+        while(sorted) {
+            sorted = false;
+            for(int i = 0; i < v.length - 1; i++) {
+                if(v[i] > v[i + 1]) {
+                    sorted = true;
+                    int tmp = v[i];
+                    v[i] = v[i + 1];
+                    v[i + 1] = tmp;
+                }
+            }
+
+        }
+    }
+    public static int[] bubbleSortCopy(int[] v){
+        int[] copy = Arrays.copyOf(v, v.length);
+        bubbleSort(copy);
+        return copy;
+    }
 }
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.myjavaoop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.myjavaoop to javafx.fxml;
-    exports com.example.myjavaoop;
-}
